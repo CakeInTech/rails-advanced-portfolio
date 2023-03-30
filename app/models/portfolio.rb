410605575785cfd5505382306fbfb28b09f220af
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-03-30 20:41:05 +0200

    - Implemented validations for blogs, skills, and portfolio item

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,6 @@
 class Portfolio < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :main_image, presence: true
+  validates :thumb_image, presence: true
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+  validates :title, presence: true
+  validates :percent_utilized, presence: true 
 end
