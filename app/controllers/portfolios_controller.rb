87377726eefb324a6158e621bbdff272b0204a5a
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-03-30 12:12:30 +0200

    - Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,21 @@
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+   @portfolio_items = Portfolio.new
+  end
+
+  def create
+    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to portfolios_path, notice: 'Your post is now live.'}
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
