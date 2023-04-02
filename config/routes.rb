21aad106c1a649ee1f98bd1d7ed63d6b0e31bb4a
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-04-02 15:13:12 +0200

    - Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
