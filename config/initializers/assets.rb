061369885fea38546e3adb55a0385e6b9e56fb48
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-04-13 13:35:22 +0200

    -  Install bootstrap to the project with cssbundling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,9 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
+Rails.application.config.assets.precompile += %w( portfolios.bootstrap.scss )
+Rails.application.config.assets.precompile += %w( blog.bootstrap.scss )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
