f7892708b5d5957b5c1fa2061efa939898fe8f21
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-04-15 23:49:40 +0200

    - Implemented styles for the initial blog layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def login_helper style
+  def login_helper style = ""
     if current_user.is_a?(GuestUser)
       (link_to 'Register', new_user_registration_path, class: style) + " ".html_safe +
       (link_to 'Login', new_user_session_path, class: style)
     else
-      button_to 'Logout', destroy_user_session_path, method: :delete, class: style
-     end
+      (button_to 'Logout', destroy_user_session_path, method: :delete, class: style)
+    end
   end
 
   def source_helper
