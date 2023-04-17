9f6b85bc8252f58b3bed2972feccd0f09c1fd461
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-04-17 19:50:29 +0200

    - Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,43 @@ module ApplicationHelper
   def copyright_generator
      CakesViewTool::Renderer.copyright "Mohamed Cake", "All rights reserved"
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+  def nav_generator style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
