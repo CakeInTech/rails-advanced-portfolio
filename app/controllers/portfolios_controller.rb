17d6c244bb84c745712f1511023b5b8bab7e74c3
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-04-17 23:09:46 +0200

    - Updated portfolio to include attr for position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,7 +3,7 @@ class PortfoliosController < ApplicationController
   layout "portfolio"
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
 
   def new
---
+++ db/migrate/20230417191026_add_postition_to_portfolios.rb
@@ -0,0 +1,5 @@
+class AddPostitionToPortfolios < ActiveRecord::Migration[7.0]
+  def change
+    add_column :portfolios, :position, :integer
+  end
+end
