48c71082a6bbc4512ff3696b12950528ee33e6b8
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-04-18 01:05:59 +0200

    - Installed jquery ui, sortable html5 library, and css calls

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,13 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import "./bootstrap_initialization";
+import { flip } from "@popperjs/core";
+import "./html.sortable";
+import "./src/jquery"
+import "./src/jquery-ui"
+
+
+$(function() {
+    console.log("hello world")
+    $( "#dialog" ).dialog();
+})
