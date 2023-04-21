944a48c57adc60ccf8cba80f1acd7c1d482deb83
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-04-21 04:15:19 +0200

    - Integrate helpers for portfolio images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -22,10 +22,4 @@ class Portfolio < ApplicationRecord
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on rails')}
 
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
-    self.thumb_image ||= Placeholder.image_generator(height: "350", width: "200")
-  end
 end
