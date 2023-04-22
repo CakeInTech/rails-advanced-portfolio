46529053c3656796b9b1414d85916eb492b71390
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-04-23 01:01:45 +0200

    Revert "- Refactored alerts into helper method"
    This reverts commit c779c8764587470373be0574b54c08b7348857f7.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,17 +57,4 @@ nav_links.html_safe
   def active? path
     "active" if current_page? path
   end
-
-  def alerts
-    alert = (flash[:alert] || flash[:error] || flash[:notice])
-
-    if alert
-      alert_generator alert
-    end
-  end
-
-  def alert_generator msg
-    js add_gritter(msg, :title => "Mohamed Cake Portfolio", sticky: false)
-  end
-
 end
